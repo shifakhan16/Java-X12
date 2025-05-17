7264e9028729072c851006b574887cd5c03e0252
shifakhan16 <shifukhan1606@gmail.com>
2025-05-17 15:51:24 +0530

    added one system out print

--- src/ex_12_Java_basics/HelloWorld.java
+++ src/ex_12_Java_basics/HelloWorld.java
@@ -3,7 +3,7 @@ package ex_12_Java_basics;
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello World");
-
+        System.out.println("I am Iron man");
     }
 
 }
